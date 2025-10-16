dd47af3ab0767fe4ca59a437811e382d9373d1da
cobotech <cobotech2@gmail.com>
2025-10-17 02:21:22 +0900

    lec4の授業内容をコミット

---
+++ lec4/js-grammar/calc.js
@@ -0,0 +1,22 @@
+/*
+let before = 1000000 ;
+ let mul = 1.06 **10;
+ let after = before * mul;
+ let answer = after - before;
+
+ document.body.append(`10年で増えたお金の総額は${answer}円です。`);
+*/
+
+/*
+ 複数行にまたがるコメントアウトの仕方
+*/
+
+/*
+document.body.append('290円の税込み価格は');
+document.body.append(290*1.1);
+document.body.append('円です');
+*/
+
+let x = 12; //円の半径
+let y = x ** 2* 3.14;
+document.body.append(`半径${x}cmの円の面積は${y}平方センチメートルです`);
